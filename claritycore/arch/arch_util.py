52526576964508d9b236add3ab54f637baae44a4
corzent <vonehsi@gmail.com>
2025-10-08 05:51:54 +0600

    add rrdbnet

--- claritycore/arch/arch_util.py
+++ claritycore/arch/arch_util.py
@@ -4,6 +4,7 @@ import warnings
 from typing import Any, Tuple, TypeVar
 
 import torch
+from torch import nn
 
 
 # --- Tuple Utilities ---
@@ -103,3 +104,34 @@ def trunc_normal_(
         The initialized tensor.
     """
     return _no_grad_trunc_normal_(tensor, mean, std, a, b)
+
+
+@torch.no_grad()
+def default_init_weights(module_list, scale=1, bias_fill=0, **kwargs):
+    """Initialize network weights.
+
+    Args:
+        module_list (list[nn.Module] | nn.Module): Modules to be initialized.
+        scale (float): Scale initialized weights, especially for residual
+            blocks. Default: 1.
+        bias_fill (float): The value to fill bias. Default: 0
+        kwargs (dict): Other arguments for initialization function.
+    """
+    if not isinstance(module_list, list):
+        module_list = [module_list]
+    for module in module_list:
+        for m in module.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, **kwargs)
+                m.weight.data *= scale
+                if m.bias is not None:
+                    m.bias.data.fill_(bias_fill)
+            elif isinstance(m, nn.Linear):
+                init.kaiming_normal_(m.weight, **kwargs)
+                m.weight.data *= scale
+                if m.bias is not None:
+                    m.bias.data.fill_(bias_fill)
+            elif isinstance(m, _BatchNorm):
+                init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    m.bias.data.fill_(bias_fill)
